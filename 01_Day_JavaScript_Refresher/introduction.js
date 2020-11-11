834ae85fc479d9fc1d7f65fe1536dab039b24317
Adam Bungart <abungart@gmail.com>
2020-11-11 16:31:58 -0600

    Finished Day 1 tasks

--- 01_Day_JavaScript_Refresher/introduction.js
+++ 01_Day_JavaScript_Refresher/introduction.js
@@ -1,1 +1,86 @@
 console.log("Welcome to 30DaysOfJavaScript");
+
+let firstName = "Mark";
+firstName = "Adam";
+
+const PI = 3.14;
+
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100]; // array of numbers
+const fruits = ["banana", "orange", "mango", "lemon"]; // array of strings, fruits
+const vegetables = ["Tomato", "Potato", "Cabbage", "Onion", "Carrot"]; // array of strings, vegetables
+const animalProducts = ["milk", "meat", "butter", "yoghurt"]; // array of strings, products
+const webTechs = ["HTML", "CSS", "JS", "React", "Redux", "Node", "MongDB"]; // array of web technologies
+const countries = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"]; // array of strings, countries
+
+console.log("Numbers:", numbers);
+console.log("Number of numbers:", numbers.length);
+
+console.log("Fruits:", fruits);
+console.log("Number of fruits:", fruits.length);
+
+console.log("Vegetables:", vegetables);
+console.log("Number of vegetables:", vegetables.length);
+
+console.log("Animal products:", animalProducts);
+console.log("Number of animal products:", animalProducts.length);
+
+console.log("Web technologies:", webTechs);
+console.log("Number of web technologies:", webTechs.length);
+
+console.log("Countries:", countries);
+console.log("Number of countries:", countries.length);
+
+const arr = [
+  "Adam",
+  250,
+  true,
+  { country: "United States", city: "Kansas City" },
+  { skills: ["HTML", "CSS", "JS", "React", "Java"] },
+]; // arr containing different data types
+console.log(arr);
+
+let js = "JavaScript";
+const charsInJavaScript = js.split("");
+
+console.log(charsInJavaScript); // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+
+let companiesString = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+const companies = companiesString.split(",");
+
+console.log(companies); // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+let txt =
+  "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+const words = txt.split(" ");
+
+console.log(words);
+
+console.log(webTechs); // all the array items
+console.log(webTechs.length); // => to know the size of the array, which is 7
+console.log(webTechs[0]); //  -> HTML
+console.log(webTechs[6]); //  -> MongoDB
+
+let lastIndex = webTechs.length - 1;
+console.log(webTechs[lastIndex]); // -> MongoDB
+
+webTechs.sort();
+console.log(webTechs); // ["CSS", "HTML", "JavaScript", "MongoDB", "Node", "React", "Redux"]
+
+webTechs.reverse(); // after sorting we can reverse it
+console.log(webTechs); // ["Redux", "React", "Node", "MongoDB", "JavaScript", "HTML", "CSS"]
+
+const person = {
+  firstName: "Asabeneh",
+  age: 250,
+  country: "Finland",
+  city: "Helsinki",
+  skills: ["HTML", "CSS", "JS"],
+  title: "teacher",
+  address: {
+    street: "Heitamienkatu 16",
+    pobox: 2002,
+    city: "Helsinki",
+  },
+  getPersonInfo: function () {
+    return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`;
+  },
+};
